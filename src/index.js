b46c1e0458f0534798abbd169fa3b67cb38a639f
AndreBal <32077546+AndreBal@users.noreply.github.com>
2020-03-20 14:05:57 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  for(let j = 0;j<1000;j++){
+        for (let i = 0;i<bracketsConfig.length;i++){
+        str = str.split(bracketsConfig[i][0]+bracketsConfig[i][1]).join('');}
+    }
+    if(!str) return true;
+    return false;
 }
